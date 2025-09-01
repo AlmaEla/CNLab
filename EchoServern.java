42485f120ef4fd9d8ecb34c9043e6d8144a73ae8
AlmaEla <sumaiyaalma7@gmail.com>
2025-09-01 09:14:18 +0600

    Create EchoServern.java

---
+++ EchoServern.java
@@ -0,0 +1,26 @@
+import java.net.*;
+
+public class EchoServer {
+    public static void main(String[] args) throws Exception {
+        DatagramSocket serverSocket = new DatagramSocket(9876);
+        byte[] receiveData = new byte[1024];
+        byte[] sendData;
+
+        System.out.println("Echo Server is running...");
+
+        while (true) {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+
+            String message = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            System.out.println("Received: " + message);
+
+            InetAddress clientAddress = receivePacket.getAddress();
+            int clientPort = receivePacket.getPort();
+
+            sendData = message.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort);
+            serverSocket.send(sendPacket);
+        }
+    }
+}
